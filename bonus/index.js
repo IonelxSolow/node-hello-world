984bfb2b330dea144e3a8348d92cdc5c8aac7672
IonelxSolow <ionelpotoroaca92@gmail.com>
2025-03-11 16:06:16 +0100

    add a conditional statements

--- bonus/index.js
+++ bonus/index.js
@@ -1,6 +1,16 @@
-const testoDaConvertire = process.argv[2];
-
-console.log(testoDaConvertire.toUpperCase());
 
 
+const testoDaConvertire = process.argv.slice(2); //start dal secondo elemento
 
+
+if (testoDaConvertire.length > 0) {
+    const terminalText = testoDaConvertire.join(" "); // ritorna un array di stringhe con spazio vuoto tra loro
+    console.log(terminalText.toUpperCase());
+} else {
+    console.log("Hello World");
+}
+
+
+
+//node index.js //: Hello World
+//node index.js "Hello Boolean" //: HELLO BOOLEAN
